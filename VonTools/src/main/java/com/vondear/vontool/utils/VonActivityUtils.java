43ac307ed32a13e13336b806faa65c8832ac70d5
秋逸 <vondear@163.com>
2016-10-26 17:43:21 +0800

    update data
    update data

---
+++ VonTools/src/main/java/com/vondear/vontool/utils/VonActivityUtils.java
@@ -0,0 +1,102 @@
+package com.vondear.vontool.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+/**
+ * Created by vondear on 2016/1/24.
+ */
+public class VonActivityUtils {
+
+    /**
+     * 判断是否存在指定Activity
+     *
+     * @param context     上下文
+     * @param packageName 包名
+     * @param className   activity全路径类名
+     * @return {@code true}: 是<br>{@code false}: 否
+     */
+    public static boolean isExistActivity(Context context, String packageName, String className) {
+        Intent intent = new Intent();
+        intent.setClassName(packageName, className);
+        return !(context.getPackageManager().resolveActivity(intent, 0) == null ||
+                intent.resolveActivity(context.getPackageManager()) == null ||
+                context.getPackageManager().queryIntentActivities(intent, 0).size() == 0);
+    }
+
+    /**
+     * 打开指定的Activity
+     *
+     * @param context     上下文
+     * @param packageName 包名
+     * @param className   全类名
+     */
+    public static void launchActivity(Context context, String packageName, String className) {
+        launchActivity(context, packageName, className, null);
+    }
+
+    /**
+     * 打开指定的Activity
+     *
+     * @param context     上下文
+     * @param packageName 包名
+     * @param className   全类名
+     * @param bundle      bundle
+     */
+    public static void launchActivity(Context context, String packageName, String className, Bundle bundle) {
+        context.startActivity(VonIntentUtils.getComponentNameIntent(packageName, className, bundle));
+    }
+
+
+    /**
+     * Activity 跳转
+     *
+     * @param context
+     * @param goal
+     */
+    public static void skipActivityAndFinish(Context context, Class<?> goal, Bundle bundle) {
+        Intent intent = new Intent(context, goal);
+        intent.putExtras(bundle);
+        context.startActivity(intent);
+        ((Activity) context).finish();
+    }
+
+    /**
+     * Activity 跳转
+     *
+     * @param context
+     * @param goal
+     */
+    public static void skipActivityAndFinish(Context context, Class<?> goal) {
+        Intent intent = new Intent(context, goal);
+        context.startActivity(intent);
+        ((Activity) context).finish();
+    }
+
+
+    /**
+     * Activity 跳转
+     *
+     * @param context
+     * @param goal
+     */
+    public static void skipActivity(Context context, Class<?> goal) {
+        Intent intent = new Intent(context, goal);
+        context.startActivity(intent);
+    }
+
+    /**
+     * Activity 跳转
+     *
+     * @param context
+     * @param goal
+     */
+    public static void skipActivity(Context context, Class<?> goal, Bundle bundle) {
+        Intent intent = new Intent(context, goal);
+        intent.putExtras(bundle);
+        context.startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/vondear/vontools/utils/VonUtils.java
@@ -0,0 +1,133 @@
+package com.vondear.vontools.utils;
+
+import android.app.Activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by vondear on 2016/1/24.
+ */
+public class VonUtils {
+
+    /**
+     * 点击隐藏软键盘
+     *
+     * @param activity
+     * @param view
+     */
+    public static void hideKeyboard(Activity activity, View view) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    /**
+     * 倒计时
+     *
+     * @param textView 控件
+     * @param waitTime 倒计时总时长
+     * @param interval 倒计时的间隔时间
+     * @param hint     倒计时完毕时显示的文字
+     */
+    public static void countDown(final TextView textView, long waitTime, long interval, final String hint) {
+        textView.setEnabled(false);
+        android.os.CountDownTimer timer = new android.os.CountDownTimer(waitTime, interval) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                textView.setText("剩下 " + (millisUntilFinished / 1000) + " S");
+            }
+
+            @Override
+            public void onFinish() {
+                textView.setEnabled(true);
+                textView.setText(hint);
+            }
+        };
+        timer.start();
+    }
+    //-----------------------------------------Toast 替代方法----------------------------------------
+
+    /**
+     * 封装了Toast的方法 :需要等待
+     *
+     * @param cxt
+     * @param str
+     * @param isLong
+     */
+    public static void showToast(Context cxt, String str, boolean isLong) {
+        if (isLong) {
+            Toast.makeText(cxt, str, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(cxt, str, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Toast 替代方法 ：立即显示无需等待
+     */
+    private static Toast mToast;
+
+    /**
+     * Toast 替代方法 ：立即显示无需等待
+     *
+     * @param context  实体
+     * @param resId    资源ID
+     * @param duration 显示时长
+     */
+    public static void showToast(Context context, int resId, int duration) {
+        showToast(context, context.getString(resId), duration);
+    }
+
+    /**
+     * Toast 替代方法 ：立即显示无需等待
+     *
+     * @param context  实体
+     * @param msg      要显示的字符串
+     * @param duration 显示时长
+     */
+    public static void showToast(Context context, String msg, int duration) {
+        if (mToast == null) {
+            mToast = Toast.makeText(context, msg, duration);
+        } else {
+            mToast.setText(msg);
+        }
+        mToast.show();
+    }
+    //===========================================Toast 替代方法======================================
+
+    /**
+     * 手动计算出listView的高度，但是不再具有滚动效果
+     *
+     * @param listView
+     */
+    public static void fixListViewHeight(ListView listView) {
+        // 如果没有设置数据适配器，则ListView没有子项，返回。
+        ListAdapter listAdapter = listView.getAdapter();
+        int totalHeight = 0;
+        if (listAdapter == null) {
+            return;
+        }
+        for (int index = 0, len = listAdapter.getCount(); index < len; index++) {
+            View listViewItem = listAdapter.getView(index, null, listView);
+            // 计算子项View 的宽高
+            listViewItem.measure(0, 0);
+            // 计算所有子项的高度
+            totalHeight += listViewItem.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        // listView.getDividerHeight()获取子项间分隔符的高度
+        // params.height设置ListView完全显示需要的高度
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+    }
+
+}
