d295429e77fe6c0f295e89aa3cec2038773b3251
秋逸 <vondear@163.com>
2017-03-03 17:27:18 +0800

    update RxSPUtils

--- RxTools-library/src/main/java/com/vondear/rxtools/RxSPUtils.java
+++ RxTools-library/src/main/java/com/vondear/rxtools/RxSPUtils.java
@@ -3,14 +3,15 @@ package com.vondear.rxtools;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import java.util.Map;
-
 /**
  * SharedPreferences工具类
  * Created by vonde on 2016/1/24.
  */
 
 public class RxSPUtils {
+
+    private final static String JSON_CACHE = "JSON_CACHE";
+
     /**
      * 存入自定义的标识的数据 可以近似的看作网络下载数据的缓存
      * 单条方式存入
@@ -20,10 +21,7 @@ public class RxSPUtils {
      * @param content 存入的内
      */
     public static void putContent(Context context, String tag, String content) {
-        SharedPreferences sp = context.getSharedPreferences(tag, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putString(tag, content);
-        editor.commit();
+        putString(context, tag, content);
     }
 
     /**
@@ -34,35 +32,171 @@ public class RxSPUtils {
      * @return 返回以tag区分的内容，默认为空
      */
     public static String getContent(Context context, String tag) {
-        SharedPreferences sp = context.getSharedPreferences(tag, Context.MODE_PRIVATE);
-        String myF = sp.getString(tag, "");
-        return myF;
+        return getString(context, tag);
     }
 
+    /**
+     * SP中写入String类型value
+     *
+     * @param key   键
+     * @param value 值
+     */
+    public static void putString(Context context, String key, String value) {
+        SharedPreferences sp = context.getSharedPreferences(key, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
 
-    private final static String JSON_CACHE = "json_cache";
+    /**
+     * SP中读取String
+     *
+     * @param key 键
+     * @return 存在返回对应值，不存在返回默认值{@code defaultValue}
+     */
+    public static String getString(Context context, String key) {
+        SharedPreferences sp = context.getSharedPreferences(key, Context.MODE_PRIVATE);
+        String value;
+        value = sp.getString(key, "");
+        return value;
+    }
+
+    /**
+     * SP中写入int类型value
+     *
+     * @param key   键
+     * @param value 值
+     */
+    public static void putInt(Context context, String key, int value) {
+        SharedPreferences sp = context.getSharedPreferences(key, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt(key, value);
+        editor.apply();
+    }
+
+    /**
+     * SP中读取int
+     *
+     * @param key 键
+     * @return 存在返回对应值，不存在返回默认值-1
+     */
+    public static int getInt(Context context, String key) {
+        SharedPreferences sp = context.getSharedPreferences(key, Context.MODE_PRIVATE);
+        int value;
+        value = sp.getInt(key, -1);
+        return value;
+    }
+
+    /**
+     * SP中写入long类型value
+     *
+     * @param key   键
+     * @param value 值
+     */
+    public static void putLong(Context context, String key, long value) {
+        SharedPreferences sp = context.getSharedPreferences(key, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putLong(key, value);
+        editor.apply();
+    }
+
+    /**
+     * SP中读取long
+     *
+     * @param key 键
+     * @return 存在返回对应值，不存在返回默认值-1
+     */
+    public static long getLong(Context context, String key) {
+        SharedPreferences sp = context.getSharedPreferences(key, Context.MODE_PRIVATE);
+        long value;
+        value = sp.getLong(key, -1L);
+        return value;
+    }
+
+    /**
+     * SP中写入float类型value
+     *
+     * @param key   键
+     * @param value 值
+     */
+    public static void putFloat(Context context, String key, float value) {
+        SharedPreferences sp = context.getSharedPreferences(key, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putFloat(key, value);
+        editor.apply();
+    }
+
+    /**
+     * SP中读取float
+     *
+     * @param key 键
+     * @return 存在返回对应值，不存在返回默认值-1
+     */
+    public static float getFloat(Context context, String key) {
+        SharedPreferences sp = context.getSharedPreferences(key, Context.MODE_PRIVATE);
+        float value;
+        value = sp.getFloat(key, -1F);
+        return value;
+    }
+
+    /**
+     * SP中写入boolean类型value
+     *
+     * @param key   键
+     * @param value 值
+     */
+    public static void putBoolean(Context context, String key, boolean value) {
+        SharedPreferences sp = context.getSharedPreferences(key, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
+
+    /**
+     * SP中读取boolean
+     *
+     * @param key 键
+     * @return 存在返回对应值，不存在返回默认值{@code defaultValue}
+     */
+    public static boolean getBoolean(Context context, String key) {
+        SharedPreferences sp = context.getSharedPreferences(key, Context.MODE_PRIVATE);
+        boolean value;
+        value = sp.getBoolean(key, false);
+        return value;
+    }
+
+    /**
+     * SP中移除该key
+     *
+     * @param key 键
+     */
+    public static void remove(Context context, String key) {
+        SharedPreferences sp = context.getSharedPreferences(key, Context.MODE_PRIVATE);
+        sp.edit().remove(key).apply();
+    }
+
 
     /**
      * 存放JSON缓存数据
      *
-     * @param context
-     * @param key
-     * @param content
+     * @param context 上下文
+     * @param key 键名
+     * @param content 内容
      * @return
      */
     public static void putJSONCache(Context context, String key, String content) {
         SharedPreferences sp = context.getSharedPreferences(JSON_CACHE, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
         editor.putString(key, content);
-        editor.commit();
+        editor.apply();
 
     }
 
     /**
      * 读取JSON缓存数据
      *
-     * @param context
-     * @param key
+     * @param context 上下文
+     * @param key 键名
      * @return
      */
     public static String readJSONCache(Context context, String key) {
@@ -71,11 +205,12 @@ public class RxSPUtils {
         return jsoncache;
     }
 
+
     /**
      * 清除指定的信息
      *
-     * @param context
-     * @param name
+     * @param context 上下文
+     * @param name 键名
      * @param key     若为null 则删除name下所有的键值
      */
     public static void clearPreference(Context context, String name, String key) {
@@ -86,211 +221,6 @@ public class RxSPUtils {
         } else {
             editor.clear();
         }
-        editor.commit();
-    }
-
-    private SharedPreferences sp;
-    private SharedPreferences.Editor editor;
-
-    /**
-     * SPUtils构造函数
-     *
-     * @param context 上下文
-     * @param spName  spName
-     */
-    public RxSPUtils(Context context, String spName) {
-        sp = context.getSharedPreferences(spName, Context.MODE_PRIVATE);
-        editor = sp.edit();
         editor.apply();
-    }
-
-    /**
-     * SP中写入String类型value
-     *
-     * @param key   键
-     * @param value 值
-     */
-    public void putString(String key, String value) {
-        editor.putString(key, value).apply();
-    }
-
-    /**
-     * SP中读取String
-     *
-     * @param key 键
-     * @return 存在返回对应值，不存在返回默认值{@code null}
-     */
-    public String getString(String key) {
-        return getString(key, null);
-    }
-
-    /**
-     * SP中读取String
-     *
-     * @param key          键
-     * @param defaultValue 默认值
-     * @return 存在返回对应值，不存在返回默认值{@code defaultValue}
-     */
-    public String getString(String key, String defaultValue) {
-        return sp.getString(key, defaultValue);
-    }
-
-    /**
-     * SP中写入int类型value
-     *
-     * @param key   键
-     * @param value 值
-     */
-    public void putInt(String key, int value) {
-        editor.putInt(key, value).apply();
-    }
-
-    /**
-     * SP中读取int
-     *
-     * @param key 键
-     * @return 存在返回对应值，不存在返回默认值-1
-     */
-    public int getInt(String key) {
-        return getInt(key, -1);
-    }
-
-    /**
-     * SP中读取int
-     *
-     * @param key          键
-     * @param defaultValue 默认值
-     * @return 存在返回对应值，不存在返回默认值{@code defaultValue}
-     */
-    public int getInt(String key, int defaultValue) {
-        return sp.getInt(key, defaultValue);
-    }
-
-    /**
-     * SP中写入long类型value
-     *
-     * @param key   键
-     * @param value 值
-     */
-    public void putLong(String key, long value) {
-        editor.putLong(key, value).apply();
-    }
-
-    /**
-     * SP中读取long
-     *
-     * @param key 键
-     * @return 存在返回对应值，不存在返回默认值-1
-     */
-    public long getLong(String key) {
-        return getLong(key, -1L);
-    }
-
-    /**
-     * SP中读取long
-     *
-     * @param key          键
-     * @param defaultValue 默认值
-     * @return 存在返回对应值，不存在返回默认值{@code defaultValue}
-     */
-    public long getLong(String key, long defaultValue) {
-        return sp.getLong(key, defaultValue);
-    }
-
-    /**
-     * SP中写入float类型value
-     *
-     * @param key   键
-     * @param value 值
-     */
-    public void putFloat(String key, float value) {
-        editor.putFloat(key, value).apply();
-    }
-
-    /**
-     * SP中读取float
-     *
-     * @param key 键
-     * @return 存在返回对应值，不存在返回默认值-1
-     */
-    public float getFloat(String key) {
-        return getFloat(key, -1f);
-    }
-
-    /**
-     * SP中读取float
-     *
-     * @param key          键
-     * @param defaultValue 默认值
-     * @return 存在返回对应值，不存在返回默认值{@code defaultValue}
-     */
-    public float getFloat(String key, float defaultValue) {
-        return sp.getFloat(key, defaultValue);
-    }
-
-    /**
-     * SP中写入boolean类型value
-     *
-     * @param key   键
-     * @param value 值
-     */
-    public void putBoolean(String key, boolean value) {
-        editor.putBoolean(key, value).apply();
-    }
-
-    /**
-     * SP中读取boolean
-     *
-     * @param key 键
-     * @return 存在返回对应值，不存在返回默认值{@code false}
-     */
-    public boolean getBoolean(String key) {
-        return getBoolean(key, false);
-    }
-
-    /**
-     * SP中读取boolean
-     *
-     * @param key          键
-     * @param defaultValue 默认值
-     * @return 存在返回对应值，不存在返回默认值{@code defaultValue}
-     */
-    public boolean getBoolean(String key, boolean defaultValue) {
-        return sp.getBoolean(key, defaultValue);
-    }
-
-    /**
-     * SP中获取所有键值对
-     *
-     * @return Map对象
-     */
-    public Map<String, ?> getAll() {
-        return sp.getAll();
-    }
-
-    /**
-     * SP中移除该key
-     *
-     * @param key 键
-     */
-    public void remove(String key) {
-        editor.remove(key).apply();
-    }
-
-    /**
-     * SP中是否存在该key
-     *
-     * @param key 键
-     * @return {@code true}: 存在<br>{@code false}: 不存在
-     */
-    public boolean contains(String key) {
-        return sp.contains(key);
-    }
-
-    /**
-     * SP中清除所有数据
-     */
-    public void clear() {
-        editor.clear().apply();
     }
 }
--- app/src/main/java/com/vondear/tools/activity/ActivityMovieSeat.java
+++ app/src/main/java/com/vondear/tools/activity/ActivityMovieSeat.java
@@ -1,11 +1,14 @@
 package com.vondear.tools.activity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
 
 import com.vondear.rxtools.RxBarUtils;
 import com.vondear.rxtools.activity.ActivityBase;
 import com.vondear.rxtools.view.RxMovieSeatTable;
+import com.vondear.rxtools.view.RxTitle;
+import com.vondear.rxtools.view.RxToast;
 import com.vondear.tools.R;
 
 import butterknife.BindView;
@@ -17,6 +20,8 @@ public class ActivityMovieSeat extends ActivityBase {
     RxMovieSeatTable mSeatView;
     @BindView(R.id.activity_movie_seat)
     LinearLayout mActivityMovieSeat;
+    @BindView(R.id.rx_title)
+    RxTitle mRxTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +34,14 @@ public class ActivityMovieSeat extends ActivityBase {
     }
 
 
-
     protected void initView() {
+        mRxTitle.setLeftOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RxToast.info(mContext,"退出成功").show();
+            }
+        });
+
         mSeatView.setScreenName("3号厅荧幕");//设置屏幕名称
         mSeatView.setMaxSelected(8);//设置最多选中
 
