a6a8d8a18907f788dada25dc8b4a162ceec69477
秋逸 <vondear@163.com>
2017-04-13 23:53:44 +0800

    完善Zbar加载模式

--- RxTools-library/src/main/java/com/zbar/lib/ZbarManager.java
+++ RxTools-library/src/main/java/com/zbar/lib/ZbarManager.java
@@ -7,9 +7,9 @@ package com.zbar.lib;
 public class ZbarManager {
 
 	static {
-//		System.loadLibrary("zbar");
-		System.load("data/data/com.vondear.rxtools/lib/libZBarDecoder.so");
-		System.load("data/data/com.vondear.rxtools/lib/libzbar.so");
+		System.loadLibrary("zbar");
+/*		System.load("data/data/com.vondear.rxtools/lib/libZBarDecoder.so");
+		System.load("data/data/com.vondear.rxtools/lib/libzbar.so");*/
 	}
 
 	public native String decode(byte[] data, int width, int height, boolean isCrop, int x, int y, int cwidth, int cheight);
