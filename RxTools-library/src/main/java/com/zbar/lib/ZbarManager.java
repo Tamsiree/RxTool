94fd1a1bbd182c40d325b17399fc9aa534305f26
秋逸 <vondear@163.com>
2017-04-13 21:27:14 +0800

    完善zbar解析方式

--- RxTools-library/src/main/java/com/zbar/lib/ZbarManager.java
+++ RxTools-library/src/main/java/com/zbar/lib/ZbarManager.java
@@ -7,7 +7,8 @@ package com.zbar.lib;
 public class ZbarManager {
 
 	static {
-		System.loadLibrary("zbar");
+//		System.loadLibrary("zbar");
+		System.load("data/data/com.vondear.rxtools/lib/libzbar.so");
 	}
 
 	public native String decode(byte[] data, int width, int height, boolean isCrop, int x, int y, int cwidth, int cheight);
