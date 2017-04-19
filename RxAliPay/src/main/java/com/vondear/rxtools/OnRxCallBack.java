93ca69fbd260c2a0b1bceed14241157ee69c41ac
秋逸 <vondear@163.com>
2017-04-19 13:27:33 +0800

    添加支付宝支付工具

---
+++ RxAliPay/src/main/java/com/vondear/rxtools/OnRxCallBack.java
@@ -0,0 +1,11 @@
+package com.vondear.rxtools;
+
+/**
+ * Created by Administrator on 2017/4/19.
+ */
+
+public interface OnRxCallBack {
+    void onSuccess(String s);
+
+    void onError(String s);
+}
