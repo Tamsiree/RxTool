6d25e10eeee1b9555530e7f1f1a6f42c25ee3e6a
秋逸 <vondear@163.com>
2018-04-13 16:47:38 +0800

    update zxing.

--- RxTools-library/src/main/java/com/vondear/rxtools/module/scaner/DecodeHandler.java
+++ RxTools-library/src/main/java/com/vondear/rxtools/module/scaner/DecodeHandler.java
@@ -125,8 +125,9 @@ final class DecodeHandler extends Handler {
         //modify here
         byte[] rotatedData = new byte[data.length];
         for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++)
+            for (int x = 0; x < width; x++) {
                 rotatedData[x * height + height - y - 1] = data[x + y * width];
+            }
         }
         int tmp = width;// Here we are swapping, that's the difference to #11
         width = height;
