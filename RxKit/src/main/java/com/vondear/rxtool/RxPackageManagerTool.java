2cb6d6d359936669aedf589ff6cc47adef0eb6cb
秋逸 <vondear@163.com>
2018-06-12 11:18:18 +0800

    新增跳转高德/百度地图导航工具

---
+++ RxKit/src/main/java/com/vondear/rxtool/RxPackageManagerTool.java
@@ -0,0 +1,37 @@
+package com.vondear.rxtool;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author vondear
+ * @date 2018/5/2 15:00:00
+ */
+public class RxPackageManagerTool {
+
+    public static List<PackageInfo> initPackageManager(Context mContext) {
+        PackageManager mPackageManager = mContext.getPackageManager();
+        return mPackageManager.getInstalledPackages(0);
+    }
+
+    /**
+     * 包名是否存在
+     * @param mContext 实体
+     * @param packageName 待检测 包名
+     * @return 结果
+     */
+    public static boolean haveExistPackageName(Context mContext,String packageName) {
+        List<PackageInfo> packageInfos =  initPackageManager(mContext);
+        List<String> mPackageNames = new ArrayList<>();
+        if (packageInfos != null) {
+            for (int i = 0; i < packageInfos.size(); i++) {
+                mPackageNames.add(packageInfos.get(i).packageName);
+            }
+        }
+        return mPackageNames.contains(packageName);
+    }
+}
