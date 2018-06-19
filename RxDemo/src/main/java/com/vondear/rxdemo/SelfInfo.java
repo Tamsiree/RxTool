155da0fc3c285d83489adc14af827ba57572cb0e
秋逸 <vondear@163.com>
2018-06-19 11:02:22 +0800

    更新配置信息

---
+++ RxDemo/src/main/java/com/vondear/rxdemo/SelfInfo.java
@@ -0,0 +1,13 @@
+package com.vondear.rxdemo;
+
+/**
+ * @author Vondear
+ * @date 2018/6/14
+ */
+public class SelfInfo {
+
+    public static final String ALIPAY_APPID = "";
+
+    public static final String ALIPAY_RSA2_PRIVATE = "";
+
+}
