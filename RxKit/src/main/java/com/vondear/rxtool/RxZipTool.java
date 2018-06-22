b7f930b6a50d45c5d56b53593efb0eb7a7854c52
秋逸 <vondear@163.com>
2018-06-22 16:13:00 +0800

    优化Zip4j工具

--- RxKit/src/main/java/com/vondear/rxtool/RxZipTool.java
+++ RxKit/src/main/java/com/vondear/rxtool/RxZipTool.java
@@ -1,8 +1,14 @@
 package com.vondear.rxtool;
 
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
+
 import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.model.FileHeader;
 import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.progress.ProgressMonitor;
 import net.lingala.zip4j.util.Zip4jConstants;
 
 import java.io.BufferedInputStream;
@@ -693,5 +699,93 @@ public class RxZipTool {
             return false;
         }
 
+    }
+
+    public static void Unzip(final File zipFile, String dest, String passwd,
+                             String charset, final Handler handler, final boolean isDeleteZipFile) {
+        try {
+            net.lingala.zip4j.core.ZipFile zFile = new net.lingala.zip4j.core.ZipFile(zipFile);
+            if (TextUtils.isEmpty(charset)) {
+                charset = "UTF-8";
+            }
+            zFile.setFileNameCharset(charset);
+            if (!zFile.isValidZipFile()) {
+                throw new ZipException(
+                        "Compressed files are not illegal, may be damaged.");
+            }
+            File destDir = new File(dest); // Unzip directory
+            if (destDir.isDirectory() && !destDir.exists()) {
+                destDir.mkdir();
+            }
+            if (zFile.isEncrypted()) {
+                zFile.setPassword(passwd.toCharArray());
+            }
+
+            final ProgressMonitor progressMonitor = zFile.getProgressMonitor();
+
+            Thread progressThread = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    Bundle bundle = null;
+                    Message msg = null;
+                    try {
+                        int percentDone = 0;
+                        // long workCompleted=0;
+                        // handler.sendEmptyMessage(ProgressMonitor.RESULT_SUCCESS)
+                        if (handler == null) {
+                            return;
+                        }
+                        handler.sendEmptyMessage(CompressStatus.START);
+                        while (true) {
+                            Thread.sleep(1000);
+
+                            percentDone = progressMonitor.getPercentDone();
+                            bundle = new Bundle();
+                            bundle.putInt(CompressKeys.PERCENT, percentDone);
+                            msg = new Message();
+                            msg.what = CompressStatus.HANDLING;
+                            msg.setData(bundle);
+                            handler.sendMessage(msg);
+                            if (percentDone >= 100) {
+                                break;
+                            }
+                        }
+                        handler.sendEmptyMessage(CompressStatus.COMPLETED);
+                    } catch (InterruptedException e) {
+                        bundle = new Bundle();
+                        bundle.putString(CompressKeys.ERROR, e.getMessage());
+                        msg = new Message();
+                        msg.what = CompressStatus.ERROR;
+                        msg.setData(bundle);
+                        handler.sendMessage(msg);
+                        e.printStackTrace();
+                    } finally {
+                        if (isDeleteZipFile) {
+                            zipFile.deleteOnExit();//zipFile.delete();
+                        }
+                    }
+                }
+            });
+
+            progressThread.start();
+            zFile.setRunInThread(true);
+            zFile.extractAll(dest);
+        } catch (ZipException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public class CompressStatus {
+        public final static int START = 0;
+        public final static int HANDLING = 1;
+        public final static int COMPLETED = 2;
+        public final static int ERROR = 3;
+    }
+
+    public class CompressKeys {
+        public final static String PERCENT = "PERCENT";
+        public final static String ERROR = "ERROR";
     }
 }
