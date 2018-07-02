5f5b07c8946c513dc4d5db13f057f138211a800a
秋逸 <vondear@163.com>
2018-07-02 15:35:46 +0800

    更新ArcGis工具类

--- RxArcGisKit/src/main/java/com/vondear/rxarcgiskit/tool/RxArcGisMapTool.java
+++ RxArcGisKit/src/main/java/com/vondear/rxarcgiskit/tool/RxArcGisMapTool.java
@@ -73,7 +73,7 @@ public class RxArcGisMapTool {
 
     public static void initAcrMap(final Context mContext, final MapView mMapView, final RxMapScaleView mScaleView) {
         ArcGISRuntimeEnvironment.setLicense("runtimelite,1000,rud5954166547,none,2K0RJAY3FLGP9KB10136");
-
+        mScaleView.setMapView(mMapView);
         mMapView.addMapScaleChangedListener(new MapScaleChangedListener() {
             @Override
             public void mapScaleChanged(MapScaleChangedEvent mapScaleChangedEvent) {
