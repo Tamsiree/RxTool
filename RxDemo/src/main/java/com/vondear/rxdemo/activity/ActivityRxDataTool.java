39dd7c8bdace94f6293612deb16d0aaed7b6633c
Vondear <Vondear@163.com>
2019-04-22 12:41:47 +0800

    update RxDemo.

--- RxDemo/src/main/java/com/vondear/rxdemo/activity/ActivityRxDataTool.java
+++ RxDemo/src/main/java/com/vondear/rxdemo/activity/ActivityRxDataTool.java
@@ -138,6 +138,10 @@ public class ActivityRxDataTool extends ActivityBase {
     EditText mEdString;
     @BindView(R.id.rx_title)
     RxTitle mRxTitle;
+    @BindView(R.id.btn_getPYAllFirstLetter)
+    Button mBtnGetPYAllFirstLetter;
+    @BindView(R.id.tv_getPYAllFirstLetter)
+    TextView mTvGetPYAllFirstLetter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -147,7 +151,32 @@ public class ActivityRxDataTool extends ActivityBase {
         mRxTitle.setLeftFinish(mContext);
     }
 
-    @OnClick({R.id.btn_null, R.id.btn_null_obj, R.id.btn_is_integer, R.id.btn_is_double, R.id.btn_is_number, R.id.btn_astro, R.id.btn_hind_mobile, R.id.btn_format_bank_card, R.id.btn_format_bank_card_4, R.id.btn_getAmountValue, R.id.btn_getRoundUp, R.id.btn_string_to_int, R.id.btn_string_to_long, R.id.btn_string_to_double, R.id.btn_string_to_float, R.id.btn_string_to_two_number, R.id.btn_upperFirstLetter, R.id.btn_lowerFirstLetter, R.id.btn_reverse, R.id.btn_toDBC, R.id.btn_toSBC, R.id.btn_oneCn2ASCII, R.id.btn_oneCn2PY, R.id.btn_getPYFirstLetter, R.id.btn_cn2PY})
+    @OnClick({R.id.btn_null,
+            R.id.btn_null_obj,
+            R.id.btn_is_integer,
+            R.id.btn_is_double,
+            R.id.btn_is_number,
+            R.id.btn_astro,
+            R.id.btn_hind_mobile,
+            R.id.btn_format_bank_card,
+            R.id.btn_format_bank_card_4,
+            R.id.btn_getAmountValue,
+            R.id.btn_getRoundUp,
+            R.id.btn_string_to_int,
+            R.id.btn_string_to_long,
+            R.id.btn_string_to_double,
+            R.id.btn_string_to_float,
+            R.id.btn_string_to_two_number,
+            R.id.btn_upperFirstLetter,
+            R.id.btn_lowerFirstLetter,
+            R.id.btn_reverse,
+            R.id.btn_toDBC,
+            R.id.btn_toSBC,
+            R.id.btn_oneCn2ASCII,
+            R.id.btn_oneCn2PY,
+            R.id.btn_getPYFirstLetter,
+            R.id.btn_getPYAllFirstLetter,
+            R.id.btn_cn2PY})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btn_null:
@@ -221,6 +250,9 @@ public class ActivityRxDataTool extends ActivityBase {
                 break;
             case R.id.btn_getPYFirstLetter:
                 mTvGetPYFirstLetter.setText(RxDataTool.getPYFirstLetter(mEdString.getText().toString()));
+                break;
+            case R.id.btn_getPYAllFirstLetter:
+                mTvGetPYAllFirstLetter.setText(RxDataTool.getPYAllFirstLetter(mEdString.getText().toString()));
                 break;
             case R.id.btn_cn2PY:
                 mTvCn2PY.setText(RxDataTool.cn2PY(mEdString.getText().toString()));
