9b39e84e3c4d5a12b24553398b2f8098da077b23
Vondear <Vondear@163.com>
2019-04-22 12:40:40 +0800

    update RxDataTool.

--- RxKit/src/main/java/com/vondear/rxtool/RxDataTool.java
+++ RxKit/src/main/java/com/vondear/rxtool/RxDataTool.java
@@ -507,6 +507,30 @@ public class RxDataTool {
     }
 
     /**
+     * 获得所有汉字的首字母
+     *
+     * @param s 汉字字符串
+     * @return 拼音
+     */
+    public static String getPYAllFirstLetter(String s) {
+        if (isNullString(s)) {
+            return "";
+        }
+        String py = "";
+        for (int i = 0; i < s.length(); i++) {
+            String first = s.substring(i, i + 1);
+            String py1 = oneCn2PY(first);
+            if (py1 != null) {
+                py += py1.substring(0, 1);
+            }
+        }
+        if (py == "") {
+            return null;
+        }
+        return py;
+    }
+
+    /**
      * 中文转拼音
      *
      * @param s 汉字字符串
