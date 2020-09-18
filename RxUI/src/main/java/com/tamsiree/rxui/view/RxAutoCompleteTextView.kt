ecd81c02ced7ad49a20bbdf95b746c64287f9815
Tamsiree <tamsiree.com@gmail.com>
2020-09-18 18:37:37 +0800

    Update build.gradle

--- RxUI/src/main/java/com/tamsiree/rxui/view/RxAutoCompleteTextView.kt
+++ RxUI/src/main/java/com/tamsiree/rxui/view/RxAutoCompleteTextView.kt
@@ -12,9 +12,9 @@ import androidx.appcompat.widget.AppCompatAutoCompleteTextView
  * 解决没有输入内容时也能展示筛选内容的需求
  */
 class RxAutoCompleteTextView : AppCompatAutoCompleteTextView {
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context?) : super(context!!)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context!!, attrs, defStyleAttr)
 
     override fun enoughToFilter(): Boolean {
         return true
